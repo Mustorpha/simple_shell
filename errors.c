09035ab804f707d755933d174cc23800ceb38295
~thebigmuse~ <mustorphajamiu@gmail.com>
2023-09-20 19:36:52 +0100

    Update errors.c

--- errors.c
+++ errors.c
@@ -1,13 +1,8 @@
 #include "shell.h"
 
-int num_len(int num);
-char *_itoa(int num);
-int create_error(char **args, int err);
-
 /**
  * num_len - Counts the digit length of a number.
  * @num: The number to measure.
- *
  * Return: The digit length.
  */
 int num_len(int num)
@@ -36,7 +31,6 @@ int num_len(int num)
 /**
  * _itoa - Converts an integer to a string.
  * @num: The integer.
- *
  * Return: The converted string.
  */
 char *_itoa(int num)
@@ -76,7 +70,6 @@ char *_itoa(int num)
  * create_error - Writes a custom error message to stderr.
  * @args: An array of arguments.
  * @err: The error value.
- *
  * Return: The error value.
  */
 int create_error(char **args, int err)
