98bc2be5ebadf391f2537aa3eb8c9a38f680da14
~thebigmuse~ <mustorphajamiu@gmail.com>
2023-09-20 19:33:42 +0100

    New changes

--- environ.c
+++ environ.c
@@ -1,14 +1,8 @@
 #include "shell.h"
 
-char **_copyenv(void);
-void free_env(void);
-char **_getenv(const char *var);
-
 /**
  * _copyenv - Creates a copy of the environment.
- *
  * Return: If an error occurs - NULL.
- *         O/w - a double pointer to the new copy.
  */
 char **_copyenv(void)
 {
@@ -56,9 +50,7 @@ void free_env(void)
 /**
  * _getenv - Gets an environmental variable from the PATH.
  * @var: The name of the environmental variable to get.
- *
  * Return: If the environmental variable does not exist - NULL.
- *         Otherwise - a pointer to the environmental variable.
  */
 char **_getenv(const char *var)
 {
