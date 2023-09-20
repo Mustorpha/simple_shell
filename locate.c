174d288adf9e3dcd173658639d72a5960945e6e5
~thebigmuse~ <mustorphajamiu@gmail.com>
2023-09-20 19:43:39 +0100

    Update locate.c

--- locate.c
+++ locate.c
@@ -1,14 +1,9 @@
 #include "shell.h"
 
-char *fill_path_dir(char *path);
-list_t *get_path_dir(char *path);
-
 /**
  * get_location - Locates a command in the PATH.
  * @command: The command to locate.
- *
  * Return: If an error occurs or the command cannot be located - NULL.
- *         Otherwise - the full pathname of the command.
  */
 char *get_location(char *command)
 {
@@ -50,11 +45,8 @@ char *get_location(char *command)
 
 /**
  * fill_path_dir - Copies path but also replaces leading/sandwiched/trailing
- *		   colons (:) with current working directory.
  * @path: The colon-separated list of directories.
- *
  * Return: A copy of path with any leading/sandwiched/trailing colons replaced
- *	   with the current working directory.
  */
 char *fill_path_dir(char *path)
 {
@@ -105,9 +97,7 @@ char *fill_path_dir(char *path)
 
 /**
  * get_path_dir - Tokenizes a colon-separated list of
- *                directories into a list_s linked list.
  * @path: The colon-separated list of directories.
- *
  * Return: A pointer to the initialized linked list.
  */
 list_t *get_path_dir(char *path)
