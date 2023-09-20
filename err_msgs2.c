2852c7488258fdf4296a137ebfd4e0fe1d9c8b04
~thebigmuse~ <mustorphajamiu@gmail.com>
2023-09-20 19:36:14 +0100

    Update err_msgs2.c

--- err_msgs2.c
+++ err_msgs2.c
@@ -3,7 +3,6 @@
 /**
  * error_126 - Creates an error message for permission denied failures.
  * @args: An array of arguments passed to the command.
- *
  * Return: The error string.
  */
 char *error_126(char **args)
@@ -37,7 +36,6 @@ char *error_126(char **args)
 /**
  * error_127 - Creates an error message for command not found failures.
  * @args: An array of arguments passed to the command.
- *
  * Return: The error string.
  */
 char *error_127(char **args)
