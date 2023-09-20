513fb99d4e0f1b37647fa5627c3c91d945618a8c
~thebigmuse~ <mustorphajamiu@gmail.com>
2023-09-20 19:44:43 +0100

    Update proc_file_comm.c

--- proc_file_comm.c
+++ proc_file_comm.c
@@ -1,13 +1,9 @@
 #include "shell.h"
 
-int cant_open(char *file_path);
-int proc_file_commands(char *file_path, int *exe_ret);
-
 /**
  * cant_open - If the file doesn't exist or lacks proper permissions, print
  * a cant open error.
  * @file_path: Path to the supposed file.
- *
  * Return: 127.
  */
 
@@ -46,10 +42,7 @@ int cant_open(char *file_path)
  * within.
  * @file_path: Path to the file.
  * @exe_ret: Return value of the last executed command.
- *
  * Return: If file couldn't be opened - 127.
- *	   If malloc fails - -1.
- *	   Otherwise the return value of the last command ran.
  */
 int proc_file_commands(char *file_path, int *exe_ret)
 {
