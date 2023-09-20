4e60904293a8a3e61831fec6735e4884782d391e
MrVeeJon <victor.jonathan.u@gmail.com>
2023-09-20 12:58:40 -0700

    linkedlist.c retouched

--- linkedlist.c
+++ linkedlist.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 
 /**
- * add_alias_end - Adds a node to the end of a alias_t linked list.
- * @head: A pointer to the head of the list_t list.
- * @name: The name of the new alias to be added.
+ * add_alias_end - Adds a node to the end of alias_t linked list.
+ * @head: Points to the head of the list_t list.
+ * @name: The name of the new alias which is to be added.
  * @value: The value of the new alias to be added.
  * Return: If an error occurs - NULL.
  */
@@ -39,7 +39,7 @@ alias_t *add_alias_end(alias_t **head, char *name, char *value)
 }
 
 /**
- * add_node_end - Adds a node to the end of a list_t linked list.
+ * add_node_end - Adds a node to end of a list_t linked list.
  * @head: A pointer to the head of the list_t list.
  * @dir: The directory path for the new node to contain.
  * Return: If an error occurs - NULL.
